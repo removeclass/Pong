7843d4a5e48240825d84ee9e88ad34b95a19589f
removeclass <jung.fishhao@gmail.com>
2018-08-29 00:06:18 +0800

    Fixed JS

--- pong.js
+++ pong.js
@@ -1,66 +1,78 @@
 $(document).ready(function(){
 
-
+	// Setting up the game
 	var canvas = document.getElementById("canvas");
-	var ctx = canvas.getContext("2d")
+	var ctx = canvas.getContext("2d");
 	var gameOver = true;
 
+	// Setting up constants
 	const PI = Math.PI;
 	const HEIGHT = canvas.height;
 	const WIDTH = canvas.width;
 	const upKey = 38, downKey = 40;
 
+	// User Inputs
 	var keyPressed = null;
-
+	
+	// Setting up the game objects
 	var player = {
-		x:null,
-		y:null,
-		width:20,
-		height:100,
-		update:function(){
-			if(keyPressed == upKey) this.y -= 10;
-			if(keyPressed == downKey) this.y += 10;
+		x: null,
+		y: null,
+		width: 20,
+		height: 100,
 
-			}
+		update: function(){
+			// Moving the paddle according to the keyPressed
+			if(keyPressed == 38) this.y -= 10;
+			if(keyPressed == 40) this.y += 10;
 		},
-		draw:function(){
-			ctx.fillRect(this.x,this.y,this.width,this.height);
+
+		draw: function(){
+			ctx.fillRect(this.x, this.y, this.width, this.height);
 		}
 	}
 
 	var ai = {
-		x:null,
-		y:null,
-		width:20,
-		height:100,
-		update:function(){
+		x: null,
+		y: null,
+		width: 20,
+		height: 100,
+
+		update: function(){
 			let target = ball.y - (this.height - ball.size) / 2;
 			this.y += (target - this.y) * 0.1;
 		},
-		draw:function(){
-			ctx.fillRect(this.x,this.y,this.width,this.height);
+
+		draw: function(){
+			ctx.fillRect(this.x, this.y, this.width, this.height);
 		}
 	}
 
 	var ball = {
-		x:null,
-		y:null,
-		size:20,
-		speedx:null,
-		speedy:null,
-		speed:10,
-		update:function(){
+		x: null,
+		y: null,
+		size: 20,
+		speedx: null,
+		speedy: null,
+		speed: 10,
+
+		update: function(){
+			// Moving the ball
 			this.x += this.speedx;
 			this.y += this.speedy;
 
-			this(this.y + this.size >= HEIGHT || this.y <= 0){
+			// Bounce on top & bottom edge
+			if(this.y + this.size >= HEIGHT || this.y <= 0){
 				this.speedy *= -1;
 			}
 
-			function checkCollision(a,b){
-				return(a.x < b.x + b.width && a.y < b.y + b.height && b.x < a.x + a.size && b.y < a.y + a.size);
+			// Function for collision checking
+			function checkCollision(a, b){
+				// Return true of ball collide with others
+				return (a.x < b.x + b.width && a.y < b.y + b.height && b.x < a.x + a.size && b.y < a.y + a.size);
 			}
 
+			// Movement direction determines which object the ball will collide with
 			let other;
 
 			if(ball.speedx < 0){
@@ -69,11 +81,13 @@ $(document).ready(function(){
 				other = ai;
 			}
 
-			let collide = checkCollision(ball,other);
+			// Check for collision with paddle
+			let collided = checkCollision(ball, other);
 
+			// An equation for ball's moving direction when it collides with the paddle
 			if(collided){
 				let n = (this.y + this.size - other.y) / (other.height + this.size);
-				let phi = 0.25 * PI * (2 * n - 1);
+				let phi = 0.25 * PI * (2 * n - 1)
 				this.speedx = this.speed * Math.cos(phi);
 				this.speedy = this.speed * Math.sin(phi);
 				if(other == ai) this.speedx *= -1;
@@ -89,41 +103,53 @@ $(document).ready(function(){
 				}
 			}
 		},
-		draw:function(){
-			ctx.fillRect(this.x,this.y,this.size,this.size);
+
+		draw: function(){
+			ctx.fillRect(this.x, this.y, this.size, this.size);
 		}
 	}
 
+	// Main function
 	function main(){
+		// Initialize the game
 		init();
 
+		// Loop for recursive call of window.requestAnimationFrame()
 		var loop = function(){
 			update();
 			draw();
-			window.requestAnimationFrame(loop,canvas);
+			window.requestAnimationFrame(loop, canvas);
 		}
-		window.requestAnimationFrame(loop,canvas);
+		window.requestAnimationFrame(loop, canvas);
 	}
 
+	// Game Initialization for object's positions
 	function init(){
+
 		gameOver = false;
+
 		$("h1").html("Pong");
 
+		// Moving the player & the AI to the middle of the screen
 		player.x = 20;
 		player.y = (HEIGHT - player.height) / 2;
 
-		ai.x = WIDTH - ai.width - 20;
+		ai.x = (WIDTH - ai.width - 20);
 		ai.y = (HEIGHT - player.height) / 2;
 
+		// Putting the ball in the middle
 		ball.x = (WIDTH - ball.size) / 2;
 		ball.y = (HEIGHT - ball.size) / 2;
 
+		// Serving the ball
 		ball.speedx = ball.speed;
+		// This gives either 0 or 1 to serve the ball in random direction
 		if(Math.round(Math.random()))
 			ball.speedx *= -1;
-		ball.speedy  0;
+		ball.speedy = 0;
 	}
 
+	// Game Update
 	function update(){
 		if(!gameOver)
 			ball.update();
@@ -131,38 +157,46 @@ $(document).ready(function(){
 		player.update();
 	}
 
+	// Draw new frame
 	function draw(){
-		ctx.fillRect(0,0,WIDTH,HEIGHT);
-		ctx.save();
+		ctx.fillRect(0, 0, WIDTH, HEIGHT); // Fill the background black
 
+		ctx.save(); // Save current settings of drawing
+
+		// Drawing the game objects in white
 		ctx.fillStyle = "white";
 		ball.draw();
-		player.draw();
 		ai.draw();
+		player.draw();
 
+		// Optional: Drawing some white stripes for styles
 		let w = 4;
 		let x = (WIDTH - w) / 2;
 		let y = 0;
 		let step = HEIGHT / 15;
-		while(y < HEIGHT){
-			ctx.fillRect(x,y + step * 0.25, w, step * 0.5);
+		while (y < HEIGHT){
+			ctx.fillRect(x, y + step * 0.25, w, step * 0.5);
 			y += step;
 		}
-		ctx.restore();
+
+		ctx.restore(); // Restore the saved settings of drawing
 	}
 
-	$(document).on("keyup",function(){
+	// Sensing the user's key inputs
+	$(document).on("keyup", function(){
 		keyPressed = null;
 	});
 
-	$(document).on("keydown", function(){
+	$(document).on("keydown", function(e){
 		keyPressed = e.which;
 	});
 
-	$("button").on("click",function(){
+	// Restarting the game on button click
+	$("button").on("click", function(){
 		$(this).hide();
 		init();
 	})
 
+	// Calling the main function
 	main();
 });
